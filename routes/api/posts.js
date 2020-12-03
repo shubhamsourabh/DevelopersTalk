8cd60ce435d2db0052fc1cec562fcdf1fe649d3e
Shubham Sourabh <sourabhshubham007@gmail.com>
2020-12-03 19:44:00 +0530

    Post Created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
+// @Route POST api/posts
+//  @desc Create a post
+//  @acesss private
 
-// @Route GET api/posts
-//  @desc TEST Route
-//  @acesss PUBLIC
+router.post(
+	"/",
+	[auth, [check("text", "Text is Required").notEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-router.get("/", (req, res) => res.send("post Route"));
+		try {
+			const user = await User.findById(req.user.id).select("-password");
+
+			const newPost = new Post({
+				text: req.body.text,
+				name: req.user.name,
+				avatar: req.user.avatar,
+				user: req.user.id,
+			});
+			const post = await newPost.save();
+			res.json(post);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send(`Server error`);
+		}
+	}
+);
 
 module.exports = router;
